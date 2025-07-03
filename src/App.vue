69bec39cab7111ed0b81ab473d51893fdcd9e269
Sim Kuntheng <simkuntheng@gmail.com>
2025-07-03 12:57:38 +0700

    components separate

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white dark:bg-dim-900">
+    <div class="container mx-auto h-screen">
+      <div class="flex flex-row justify-center">
+        <LeftSide />
+        <Middle />
+        <RightSide />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import LeftSide from '@/components/LeftSide.vue';
+import Middle from '@/components/Middle.vue';
+import RightSide from '@/components/RightSide.vue';
+export default {
+  components: {
+    LeftSide,
+    RightSide,
+    Middle
+  }
+}
+</script>
